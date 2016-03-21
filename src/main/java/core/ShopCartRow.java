5bad5e0e895733be5465788d41dc3f3b34ddccbb
licettea <AurelienL@iMac-de-Aurelien.local>
2016-03-21 17:26:29 +0100

    ShopCart

--- src/main/java/core/ShopCartRow.java
+++ src/main/java/core/ShopCartRow.java
@@ -11,6 +11,24 @@ public class ShopCartRow {
 		
 	}
 	
+	public void setName(String name){
+		this.name = name;
+	}
 	
-
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public int getPrice() {
+		return this.price;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
 }
